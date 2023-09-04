55da691de17bd1e82ce138985d176f7ad520bf57
Jamie Feliu <jamieleefeliu@gmail.com>
2023-09-04 11:11:08 -0400

    Slider Responsive

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Notification = () => {
     return (
         <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
